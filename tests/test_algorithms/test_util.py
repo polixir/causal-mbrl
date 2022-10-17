ea8d01a8183e6905fd46d3c22622d628224bc0f2
frank <franktian424@qq.com>
2022-10-17 21:23:36 +0800

    :beetle: try to pass CI-test

--- tests/test_algorithms/test_util.py
+++ tests/test_algorithms/test_util.py
@@ -1,24 +1,7 @@
 from cmrl.algorithms.util import load_offline_data
 
-import os
-from copy import deepcopy
-from typing import Optional, cast
-
-import emei
-import hydra.utils
-import numpy as np
-from omegaconf import DictConfig
-from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.buffers import ReplayBuffer
-from torch.utils.data import DataLoader, Dataset
 
-from cmrl.agent import complete_agent_cfg
-from cmrl.algorithms.util import maybe_load_trained_offline_model, setup_fake_env
-from cmrl.models.dynamics import ConstraintBasedDynamics
-from cmrl.models.fake_env import VecFakeEnv
-from cmrl.sb3_extension.eval_callback import EvalCallback
-from cmrl.sb3_extension.logger import configure as logger_configure
-from cmrl.types import InitObsFnType, RewardFnType, TermFnType
 from cmrl.util.env import make_env
 
 from tests.utils import cfg
