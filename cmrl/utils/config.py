485515282e433a79f2a6a09191198e290354f8d8
frank <franktian424@gmail.com>
2023-01-15 12:53:27 +0800

    :tada: add exp_collect

--- cmrl/utils/config.py
+++ cmrl/utils/config.py
@@ -1,8 +1,13 @@
 import pathlib
 from typing import Dict, Union, Optional
+from collections import defaultdict
 
 import omegaconf
 from omegaconf import DictConfig
+import pandas as pd
+import numpy as np
+
+PACKAGE_PATH = pathlib.Path(__file__).parent.parent.parent
 
 
 def load_hydra_cfg(results_dir: Union[str, pathlib.Path]) -> omegaconf.DictConfig:
@@ -23,3 +28,37 @@ def load_hydra_cfg(results_dir: Union[str, pathlib.Path]) -> omegaconf.DictConfi
     if not isinstance(cfg, omegaconf.DictConfig):
         raise RuntimeError("Configuration format not a omegaconf.DictConf")
     return cfg
+
+
+def exp_collect(cfg_extractor,
+                csv_extractor,
+                env_name="ContinuousCartPoleSwingUp-v0",
+                exp_name="default",
+                exp_path=None):
+    data = defaultdict(list)
+
+    if exp_path is None:
+        exp_path = PACKAGE_PATH / "exp"
+    exp_dir = exp_path / exp_name
+    env_dir = exp_dir / env_name
+
+    for params_dir in env_dir.glob("*"):
+        for dataset_dir in params_dir.glob("*"):
+            for time_dir in dataset_dir.glob("*"):
+                if not (time_dir / ".hydra").exists():  # exp by hydra's MULTIRUN mode, multi exp in this time
+                    time_dir_list = list(time_dir.glob("*"))
+                else:  # only one exp in this time
+                    time_dir_list = [time_dir]
+
+                for single_dir in time_dir_list:
+                    if single_dir.name == "multirun.yaml":
+                        continue
+
+                    cfg = load_hydra_cfg(single_dir)
+
+                    key = cfg_extractor(cfg, params_dir.name, dataset_dir.name, time_dir.name)
+                    if not key:
+                        continue
+
+                    data[key] = csv_extractor(single_dir / "log")
+    return data
