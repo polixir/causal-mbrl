e68768d1f739fb6bca19df6fbfb2a90323991b0b
frank <franktian424@qq.com>
2023-02-28 19:02:34 +0800

    :tada: add self-adaption

--- cmrl/models/causal_mech/kernel_test.py
+++ cmrl/models/causal_mech/kernel_test.py
@@ -1,5 +1,8 @@
 from typing import Optional, List, Dict, Union, MutableMapping
+from functools import partial
+from collections import defaultdict
 
+import pathlib
 import numpy
 import numpy as np
 import torch
@@ -11,37 +14,39 @@ from tqdm import tqdm
 
 from cmrl.models.causal_mech.base import EnsembleNeuralMech
 from cmrl.utils.variables import Variable, ContinuousVariable, DiscreteVariable, BinaryVariable, RadianVariable
+from cmrl.models.graphs.binary_graph import BinaryGraph
 
 
 class KernelTestMech(EnsembleNeuralMech):
     def __init__(
-            self,
-            # base
-            name: str,
-            input_variables: List[Variable],
-            output_variables: List[Variable],
-            logger: Optional[Logger] = None,
-            # model learning
-            longest_epoch: int = -1,
-            improvement_threshold: float = 0.01,
-            patience: int = 5,
-            # ensemble
-            ensemble_num: int = 7,
-            elite_num: int = 5,
-            # cfgs
-            network_cfg: Optional[DictConfig] = None,
-            encoder_cfg: Optional[DictConfig] = None,
-            decoder_cfg: Optional[DictConfig] = None,
-            optimizer_cfg: Optional[DictConfig] = None,
-            scheduler_cfg: Optional[DictConfig] = None,
-            # forward method
-            residual: bool = True,
-            encoder_reduction: str = "sum",
-            # others
-            device: Union[str, torch.device] = "cpu",
-            # KCI
-            sample_num=2000,
-            kci_times=10
+        self,
+        # base
+        name: str,
+        input_variables: List[Variable],
+        output_variables: List[Variable],
+        logger: Optional[Logger] = None,
+        # model learning
+        longest_epoch: int = -1,
+        improvement_threshold: float = 0.01,
+        patience: int = 5,
+        # ensemble
+        ensemble_num: int = 7,
+        elite_num: int = 5,
+        # cfgs
+        network_cfg: Optional[DictConfig] = None,
+        encoder_cfg: Optional[DictConfig] = None,
+        decoder_cfg: Optional[DictConfig] = None,
+        optimizer_cfg: Optional[DictConfig] = None,
+        scheduler_cfg: Optional[DictConfig] = None,
+        # forward method
+        residual: bool = True,
+        encoder_reduction: str = "sum",
+        # others
+        device: Union[str, torch.device] = "cpu",
+        # KCI
+        sample_num=2000,
+        kci_times=10,
+        not_confident_bound=0.2,
     ):
         EnsembleNeuralMech.__init__(
             self,
@@ -65,12 +70,43 @@ class KernelTestMech(EnsembleNeuralMech):
         )
         self.sample_num = sample_num
         self.kci_times = kci_times
+        self.not_confident_bound = not_confident_bound
+
+    def kci(
+        self,
+        input_idx: int,
+        output_idx: int,
+        inputs: MutableMapping[str, numpy.ndarray],
+        outputs: MutableMapping[str, numpy.ndarray],
+        sample_indices: np.ndarray,
+    ):
+        in_name, out_name = list(inputs.keys())[input_idx], list(outputs.keys())[output_idx]
+
+        if self.residual:
+            data_x = outputs[out_name][sample_indices] - inputs[out_name.replace("next_", "")][sample_indices]
+        else:
+            data_x = outputs[out_name][sample_indices]
+
+        def deal_with_radian_input(name, data):
+            if isinstance(self.input_variables_dict[name], RadianVariable):
+                return (data + np.pi) % (2 * np.pi) - np.pi
+            else:
+                return data
+
+        data_y = deal_with_radian_input(in_name, inputs[in_name])[sample_indices]
+        data_z = [
+            deal_with_radian_input(other_in_name, in_data)[sample_indices]
+            for other_in_name, in_data in inputs.items()
+            if other_in_name != in_name
+        ]
+        data_z = np.concatenate(data_z, axis=1)
+
+        kci = KCI_CInd()
+        p_value, test_stat = kci.compute_pvalue(data_x, data_y, data_z)
+        return p_value
 
     def kci_compute_graph(
-            self,
-            inputs: MutableMapping[str, numpy.ndarray],
-            outputs: MutableMapping[str, numpy.ndarray],
-            **kwargs
+        self, inputs: MutableMapping[str, numpy.ndarray], outputs: MutableMapping[str, numpy.ndarray], **kwargs
     ):
 
         # [[0, 0, 0, 0],
@@ -82,41 +118,54 @@ class KernelTestMech(EnsembleNeuralMech):
         length = next(iter(inputs.values())).shape[0]
         sample_length = min(length, self.sample_num)
 
-        def deal_with_radian_input(name, data):
-            if isinstance(self.input_variables_dict[name], RadianVariable):
-                return (data + np.pi) % (2 * np.pi) - np.pi
-            else:
-                return data
-
-        p_values_array = np.empty((self.kci_times, self.input_var_num, self.output_var_num))
-        with tqdm(total=self.kci_times * self.input_var_num * self.output_var_num, desc="kci") as pbar:
+        init_pvalues_array = np.empty((self.kci_times, self.input_var_num, self.output_var_num))
+        with tqdm(
+            total=self.kci_times * self.input_var_num * self.output_var_num,
+            desc="init kci of {} samples".format(sample_length),
+        ) as pbar:
             for time in range(self.kci_times):
                 sample_indices = np.random.permutation(length)[:sample_length]
-                for out_idx, out_name in enumerate(outputs):
-                    for in_idx, in_name in enumerate(inputs):
-                        if self.residual:
-                            data_x = outputs[out_name][sample_indices] - inputs[out_name.replace("next_", "")][
-                                sample_indices]
-                            data_x = data_x
-                        else:
-                            data_x = outputs[out_name][sample_indices]
-                        data_y = deal_with_radian_input(in_name, inputs[in_name])[sample_indices]
-                        data_z = [deal_with_radian_input(other_in_name, in_data)[sample_indices]
-                                  for other_in_name, in_data in inputs.items() if other_in_name != in_name]
-                        data_z = np.concatenate(data_z, axis=1)
-
-                        # data_x = (data_x - data_x.mean(axis=0)) / data_x.std(axis=0)
-                        # data_y = (data_y - data_y.mean(axis=0)) / data_y.std(axis=0)
-                        # data_z = (data_z - data_z.mean(axis=0)) / data_z.std(axis=0)
-
-                        kci = KCI_CInd()
-                        p_value, test_stat = kci.compute_pvalue(data_x, data_y, data_z)
-                        p_values_array[time][in_idx][out_idx] = p_value
-
+                kci = partial(self.kci, inputs=inputs, outputs=outputs, sample_indices=sample_indices)
+                for out_idx in range(len(outputs)):
+                    for in_idx in range(len(inputs)):
+                        init_pvalues_array[time][in_idx][out_idx] = kci(in_idx, out_idx)
                         pbar.update(1)
-        final_p_values = (p_values_array < 0.05).mean(axis=0)
-        print(final_p_values)
-        pass
+
+        votes = (init_pvalues_array < 0.05).mean(axis=0)
+        is_not_confident = np.logical_and(votes > self.not_confident_bound, votes < 1 - self.not_confident_bound)
+        not_confident_list = np.array(np.where(is_not_confident)).T
+
+        print(votes)
+
+        recompute_times = 1
+        while len(not_confident_list) != 0:
+            new_sample_length = sample_length * (2**recompute_times)
+            if new_sample_length > length:
+                break
+
+            pvalues_dict = defaultdict(list)
+            with tqdm(
+                total=self.kci_times * len(not_confident_list),
+                desc="{}th re-compute kci of {} samples".format(recompute_times, new_sample_length),
+            ) as pbar:
+                for time in range(self.kci_times):
+                    sample_indices = np.random.permutation(length)[:new_sample_length]
+                    kci = partial(self.kci, inputs=inputs, outputs=outputs, sample_indices=sample_indices)
+                    for in_idx, out_idx in not_confident_list:
+                        pvalues_dict[(in_idx, out_idx)].append(kci(in_idx, out_idx))
+                        pbar.update(1)
+
+            not_confident_list = []
+            for key, value in pvalues_dict.items():
+                vote = (np.array(value) < 0.05).mean()
+                if self.not_confident_bound < vote < 1 - self.not_confident_bound:
+                    not_confident_list.append(key)
+                else:
+                    votes[key] = vote
+            print(votes)
+            recompute_times += 1
+
+        return votes > 0.5
 
     def build_network(self):
         self.network = instantiate(self.network_cfg)(
@@ -125,14 +174,27 @@ class KernelTestMech(EnsembleNeuralMech):
             extra_dims=[self.ensemble_num],
         ).to(self.device)
 
-    def forward(
-            self,
-            inputs: MutableMapping[str, numpy.ndarray]
-    ) -> Dict[str, torch.Tensor]:
+    def forward(self, inputs: MutableMapping[str, numpy.ndarray]) -> Dict[str, torch.Tensor]:
         pass
 
+    def build_graph(self):
+        self.graph = BinaryGraph(self.input_var_num, self.output_var_num, device=self.device)
 
-if __name__ == '__main__':
+    def learn(
+        self,
+        inputs: MutableMapping[str, np.ndarray],
+        outputs: MutableMapping[str, np.ndarray],
+        work_dir: Optional[Union[str, pathlib.Path]] = None,
+        **kwargs
+    ):
+        if self.discovery:
+            graph = self.kci_compute_graph(inputs, outputs)
+            self.graph.set_data(graph)
+
+        super(KernelTestMech, self).learn(inputs, outputs, work_dir=work_dir, **kwargs)
+
+
+if __name__ == "__main__":
     import gym
     from stable_baselines3.common.buffers import ReplayBuffer
     from torch.utils.data import DataLoader
@@ -144,27 +206,16 @@ if __name__ == '__main__':
     from cmrl.models.causal_mech.util import variable_loss_func
 
     env = gym.make("ContinuousCartPoleSwingUp-v0", real_time_scale=0.02)
-    real_replay_buffer = ReplayBuffer(int(1e6), env.observation_space, env.action_space, "cpu",
-                                      handle_timeout_termination=False)
-    load_offline_data(env, real_replay_buffer, "SAC-expert", use_ratio=1)
+    real_replay_buffer = ReplayBuffer(
+        int(1e6), env.observation_space, env.action_space, "cpu", handle_timeout_termination=False
+    )
+    load_offline_data(env, real_replay_buffer, "SAC-expert-replay", use_ratio=1)
 
     input_variables = parse_space(env.observation_space, "obs") + parse_space(env.action_space, "act")
     output_variables = parse_space(env.observation_space, "next_obs")
 
-    mech = KernelTestMech(
-        "kernel_test_mech",
-        input_variables,
-        output_variables,
-        sample_num=1000,
-        kci_times=20
-    )
+    mech = KernelTestMech("kernel_test_mech", input_variables, output_variables, sample_num=256, kci_times=16)
 
-    inputs, outputs = buffer_to_dict(
-        env.observation_space,
-        env.action_space,
-        env.obs2state,
-        real_replay_buffer,
-        "transition"
-    )
+    inputs, outputs = buffer_to_dict(env.observation_space, env.action_space, env.obs2state, real_replay_buffer, "transition")
 
-    mech.kci_compute_graph(inputs, outputs)
+    mech.learn(inputs, outputs)
